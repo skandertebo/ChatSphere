941a57f24e38ed52d588a32adb86640f930e76e9
skandertebo <alexandertebourb@gmail.com>
2024-01-09 16:10:15 +0100

    bug fix

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync"
@@ -9,50 +8,85 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type Connection struct {
+    Conn *websocket.Conn
+    Send chan []byte
+}
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
+
 var (
-    connections []*websocket.Conn
+    connections []*Connection
     connMutex   sync.Mutex
-    upgrader = websocket.Upgrader{
-        ReadBufferSize:  1024,
-        WriteBufferSize: 1024,
-    }
 )
 
-func listenToMessages() {
-    for {
-        connMutex.Lock()
-        for _, conn := range connections {
-            _, msg, err := conn.ReadMessage()
-            if err != nil {
-                fmt.Println(err)
+func broadcastMessage(msg []byte, sender *Connection) {
+    connMutex.Lock()
+    defer connMutex.Unlock()
+
+    for _, conn := range connections {
+        if conn != sender {
+            select {
+            case conn.Send <- msg:
+            default:
                 continue
             }
-            fmt.Println(string(msg))
-            // distribute message to all connections except the sender
-            for _, connInner := range connections {
-                if conn != connInner {
-                    if err = connInner.WriteMessage(websocket.TextMessage, msg); err != nil {
-                        fmt.Println(err)
-                        continue
-                    }
+        }
+    }
+}
+
+func handleConnection(conn *websocket.Conn) {
+    c := &Connection{
+        Conn: conn,
+        Send: make(chan []byte, 256),
+    }
+
+    connMutex.Lock()
+    connections = append(connections, c)
+    connMutex.Unlock()
+
+    go func() {
+        defer func() {
+            conn.Close()
+            connMutex.Lock()
+            for i, connection := range connections {
+                if connection == c {
+                    connections = append(connections[:i], connections[i+1:]...)
+                    break
                 }
             }
+            connMutex.Unlock()
+        }()
+
+        for {
+            _, msg, err := conn.ReadMessage()
+            if err != nil {
+                return
+            }
+            broadcastMessage(msg, c)
         }
-        connMutex.Unlock()
+    }()
+
+    for msg := range c.Send {
+        if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+            return
+        }
     }
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
     conn, err := upgrader.Upgrade(w, r, nil)
     if err != nil {
-        fmt.Println(err)
+        log.Println("Upgrade failed:", err)
         return
     }
-    connections = append(connections, conn)
+    handleConnection(conn)
 }
 
 func main() {
     http.HandleFunc("/", handler)
-    go listenToMessages()
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
